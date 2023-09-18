c40b59814295b36484a6439870054d323a1fb622
Gabrielle Fernandes <gabfernandes036@gmail.com>
2023-09-18 10:49:50 -0300

    corpo das def ler_arquivo e def ordena_lista

--- Lista 01 - #Revisão/funcoes.py
+++ Lista 01 - #Revisão/funcoes.py
@@ -18,12 +18,29 @@ def gerar_lista(quantidade:int, valor_minimo:int=1, valor_maximo:int=1000000):
 def salvar_lista(nome_lista: list, nome_arquivo: str):
     boolSucesso  = False
     nome_arquivo = DIRATUAL + '\\' + nome_arquivo
-
-
-    arquvo = open(f'{nome_arquivo}','w')
-    arquvo.write(str(nome_lista))
+    
+    arquvo = (open(f'{nome_arquivo}','w'))
+    arquvo.write(str(nome_lista)) 
     dirarq = os.path.dirname(nome_arquivo)
+    for i in range(len(nome_lista)):
+         arquvo.write(str(f"\n{nome_lista[i]}"))
     if dirarq == DIRATUAL:
         boolSucesso = True
 
     return boolSucesso
+
+def ler_arquivo (nome_arquivo:str):
+    boolSucesso = False
+    retorno = None
+
+    ...
+
+    return boolSucesso, retorno
+
+def ordena_lista (nome_lista:str,metodo_ordenna:str):
+    boolSucesso = False
+    retorno = None
+
+    ...
+
+    return boolSucesso, retorno
