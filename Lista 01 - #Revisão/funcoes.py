ddafee4fc3b12c4efcdfba8e063c658625276a4c
Gabrielle Fernandes <gabfernandes036@gmail.com>
2023-10-09 10:36:10 -0300

    Funções da questão 02

--- Lista 01 - #Revisão/funcoes.py
+++ Lista 01 - #Revisão/funcoes.py
@@ -3,6 +3,7 @@ import random, os, sys
 DIRATUAL = os.path.dirname(__file__)
 
 # ----------------------------------------------------------------------
+#Questão 01
 def gerar_lista(quantidade:int, valor_minimo:int=1, valor_maximo:int=1000000):
     boolSucesso = False
     lstRetorno  = None
@@ -14,16 +15,63 @@ def gerar_lista(quantidade:int, valor_minimo:int=1, valor_maximo:int=1000000):
     return boolSucesso, lstRetorno
 
 # ----------------------------------------------------------------------
-
+#Questão 01
 def salvar_lista(nome_lista: list, nome_arquivo: str):
     boolSucesso  = False
     nome_arquivo = DIRATUAL + '\\' + nome_arquivo
-
-
-    arquvo = open(f'{nome_arquivo}','w')
-    arquvo.write(str(nome_lista))
+    
+    arquvo = (open(f'{nome_arquivo}','w')) 
     dirarq = os.path.dirname(nome_arquivo)
+    for i in range(len(nome_lista)):
+         arquvo.write(str(f"\n{nome_lista[i]}"))
     if dirarq == DIRATUAL:
         boolSucesso = True
 
     return boolSucesso
+
+# ----------------------------------------------------------------------
+#Questão 02
+def ler_arquivo (nome_arquivo:str):
+    boolSucesso = False
+    retorno = None
+
+    lendo = open(f'{nome_arquivo}')
+    linha = open(f'{nome_arquivo}')
+    
+    lido = lendo.read()
+    linhas = len(linha.readlines())
+    tamanho = os.path.getsize(nome_arquivo)
+    contador = 0
+    sem_esp = lido.split('\n')
+    
+    for i in sem_esp:
+        if i:
+            contador += 1
+    
+    if not (tamanho == 0) or not (linhas == 0):
+        if contador == linhas:
+            boolSucesso = True
+            retorno = ''
+    else:
+        boolSucesso = False
+        retorno = None
+
+    return boolSucesso, retorno
+
+# ----------------------------------------------------------------------
+#Questão 02
+def ordena_lista (nome_lista:str,metodo_ordena:str):
+    boolSucesso = False
+    retorno = None
+
+    if metodo_ordena == 'Bubble':
+        print('Bubble')
+    elif metodo_ordena == 'Insertion':
+        print('Insertion')
+    elif metodo_ordena == 'Selection':
+        print('Selection')
+    elif metodo_ordena == 'Quick':
+        print('Quick')
+    else:
+        print('sela')
+    return boolSucesso, retorno
